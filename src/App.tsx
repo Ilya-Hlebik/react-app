4bf021798eb75c1572d46a494071d970061d2b99
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-15 21:39:06 -0600

    Form with refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 
 function App() {
@@ -7,10 +7,7 @@ function App() {
 
     return (
         <div>
-            <ExpandableText maxChars={1333330}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut ullamcorper lorem. Ut venenatis tortor nisi, eu molestie leo mattis et. Sed ac dignissim orci. Phasellus non ligula eu felis vehicula tempor. Praesent id magna quis metus maximus sollicitudin sit amet sit amet dui. Praesent rutrum tincidunt malesuada. Cras interdum nisl purus, sed dictum odio ultrices quis. Morbi feugiat rutrum fermentum.
-                Maecenas rhoncus diam vel euismod lobortis. Morbi lacus eros, porttitor at ligula a, sollicitudin molestie ante. Nam at nisl in arcu porta lobortis in at nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin accumsan, ex nec ultricies laoreet, tortor sapien rutrum nulla, id suscipit sapien sem sed elit. Morbi et vehicula odio. Pellentesque egestas pellentesque tortor at viverra.
-            </ExpandableText>
+            <Form/>
         </div>
     );
 }
