fc8f053e2d00cdc4dad07440c6cdbdcd5eb75144
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-15 22:16:49 -0600

    formState error validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,34 @@
-import {FormEvent, useState} from "react";
+import {FieldValues, useForm} from "react-hook-form";
 
-interface Props {
-
+interface FormData {
+    name: string;
+    age: number;
 }
 
-const Form = ({}: Props) => {
-    const [person, setPerson] = useState({name: '', age: ''});
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        console.log(person);
+const Form = () => {
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = (e: FieldValues) => {
+        console.log(e);
     };
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className={'mb-3'}>
                 <label htmlFor={"name"} className={'form-label'}>Name</label>
-                <input id="name" value={person.name} onChange={(event) => setPerson({...person, name: event.target.value})}  type={'text'}
+                <input id="name"
+                       {...register('name', {required: true, minLength: 3})}
+                       type={'text'}
                        className={'form-control'}/>
+                {errors.name?.type === 'required' && <p className={'text-danger'}>The name field is required</p>}
+                {errors.name?.type === 'minLength' &&
+                <p className={'text-danger'}>The name must be at least 3 characters</p>}
             </div>
             <div className={'mb-3'}>
                 <label htmlFor={"age"} className={'form-label'}>Age</label>
-                <input id="age" value={person.age} onChange={(event) => setPerson({...person, age: event.target.value})}
+                <input id="age"
+                       {...register('age')}
                        type={'number'}
-                           className={'form-control'}/>
+                       className={'form-control'}/>
             </div>
             <button className={'btn btn-primary'} type={'submit'}>Submit</button>
         </form>
