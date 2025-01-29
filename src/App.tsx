97b55d2fb0557ff61bc2361c657bf9422fcd9305
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-28 22:28:37 -0600

    axios get

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
+interface User {
+    id: number;
+    name: string;
+}
 
 function App() {
 
-    const connect = () => console.log("Connecting");
-    const disconnect = () => console.log("Disconnecting");
-    useEffect(() => {
-        connect();
-        return () => disconnect();
-    });
-    return (
-        <div>
+    const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState<string>('');
 
-        </div>
-    );
+
+    useEffect(() => {
+        axios.get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+            .then((res) => setUsers(res.data))
+            .catch(err => setError(err.message))
+    }, []);
+    return (
+        <>
+            {error && <p className={'text-danger'}>{error}</p>}
+            <ul>
+                {users.map(user => {
+                    return <li key={user.id}>{user.name}</li>
+                })}
+            </ul>
+        </>
+    )
+        ;
 }
 
 export default App;
