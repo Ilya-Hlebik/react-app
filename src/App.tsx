45950c6637e599b6d3dae1224875eb791045831e
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-28 22:14:25 -0600

    Use effect mount/unmount

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React, {useState} from "react";
-import ProductList from "./components/ProductList";
+import React, {useEffect} from "react";
 
 
 function App() {
 
-    const [category, setCategory] = useState('');
-
+    const connect = () => console.log("Connecting");
+    const disconnect = () => console.log("Disconnecting");
+    useEffect(() => {
+        connect();
+        return () => disconnect();
+    });
     return (
         <div>
-            <select className={'form-select'} onChange={(event => setCategory(event.target.value))}>
-                <option value={''}/>
-                <option value={'Clothing'}>Clothing</option>
-                <option value={'Household'}>Household</option>
-            </select>
-            <ProductList category={category}/>
+
         </div>
     );
 }
