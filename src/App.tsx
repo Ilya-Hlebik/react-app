3b1bbd9660b688f1453cab1c8ee7c65b1d979aec
Ilya-Hlebik <felix.2894@mail.ru>
2025-02-03 21:41:44 -0600

    BE example with custom hooks and http service

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import React, {useEffect, useState} from "react";
-import axios, {AxiosError} from "axios";
-
-interface User {
-    id: number;
-    name: string;
-}
+import React from "react";
+import userService, {User} from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
+    const {users, error, isLoading, setUsers, setError} = useUsers();
 
-    const [users, setUsers] = useState<User[]>([]);
-    const [error, setError] = useState<string>('');
+    const deleteUser = (user: User) => {
+        const originalUsers = users;
+        setUsers(users.filter(existingUser => existingUser.id !== user.id))
+        userService.delete(user.id)
+            .catch(err => {
+                setError(err.message);
+                setUsers(originalUsers);
+            })
+    };
+    const addUser = () => {
+        const originalUsers = users;
+        const newUser = {id: 0, name: 'Mosh'}
+        setUsers([newUser, ...users]);
+        userService.create(newUser)
+            .then(({data: savedUser}) => setUsers([savedUser, ...users]))
+            .catch(err => {
+                setError(err.message);
+                setUsers(originalUsers);
+            })
+    };
 
+    const updateUser = (user: User) => {
+        const originalUsers = users;
+        const updatedUser = {...user, name: user.name + '!'}
+        setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+        userService.update(updatedUser)
+            .catch(err => {
+                setError(err.message);
+                setUsers(originalUsers);
+            })
+    };
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const res = await axios.get<User[]>("https://jsonplaceholder.typicode.com/xusers");
-                setUsers(res.data);
-            } catch (err) {
-                setError((err as AxiosError).message)
-            }
-        };
-        fetchUsers();
-    }, []);
     return (
         <>
             {error && <p className={'text-danger'}>{error}</p>}
-            <ul>
+            {isLoading && <div className={'spinner-border'}/>}
+            <button className={'btn btn-primary mb-3'} onClick={addUser}>Add</button>
+            <ul className={'list-group'}>
                 {users.map(user => {
-                    return <li key={user.id}>{user.name}</li>
+                    return (
+                        <li key={user.id} className={'list-group-item d-flex justify-content-between'}>{user.name}
+                            <div>
+                                <button className={'btn btn-outline-secondary mx-1'}
+                                        onClick={() => updateUser(user)}>Update
+                                </button>
+                                <button className={'btn btn-outline-danger'} onClick={() => deleteUser(user)}>Delete
+                                </button>
+                            </div>
+                        </li>)
                 })}
             </ul>
         </>
